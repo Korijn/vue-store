fe8ebb6e52aead14caa9179910cf9f00b61f1253
Korijn van Golen <k.vangolen@clinicalgraphics.com>
2022-02-18 20:41:14 +0100

    lint

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,12 @@
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
-  configureWebpack: config => {
+  configureWebpack: (config) => {
     if (process.env.NODE_ENV === 'production') {
+      // eslint-disable-next-line no-param-reassign
       config.output.libraryExport = 'default';
+      // eslint-disable-next-line no-param-reassign
       config.externals = [nodeExternals()];
     }
-  }
+  },
 };
